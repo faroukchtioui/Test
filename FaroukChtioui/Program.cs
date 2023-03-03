9149544771a4cd9b3b31029e24ed50bc9d9acacb
unknown <farouk.chtioui@esprit.tn>
2023-03-03 14:25:49 +0100

    Test

---
+++ FaroukChtioui/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FaroukChtioui
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            double prixLaitPoudre = 10 * (1-0.3);
+            double prixCafe = 30 * (1 - 0.3);
+            double prixChocolat = 40 * (1 - 0.3);
+            double prixThe = 30 * (1 - 0.3);
+            double prixEau = 5 * (1 - 0.3);
+
+            double prixEspresso = 2 * prixEau + prixCafe;
+            double prixLait = 2 * prixLaitPoudre + prixEau;
+            double prixCapuccino = 2 * prixLaitPoudre + prixCafe + prixChocolat + prixEau;
+            double prixChocolatChaud = 3 * prixEau + 2 * prixChocolat;
+            double prixCafeAuLait = prixLaitPoudre + 2 * prixEau + prixCafe;
+            double prixMokaccino = prixLaitPoudre + 2 * prixEau + prixCafe + 2 * prixChocolat;
+            double prixThé = 2 * prixEau + prixThe;
+            
+
+
+            int choixBoisson;
+            string[] boissons = { "espresso", "lait", "capuccino", "chocolat chaud", "café au lait", "mokaccino", "thé" };
+
+            Console.WriteLine("Veuillez choisir une boisson :");
+            for (int i = 0; i < boissons.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}. {boissons[i]}");
+            }
+            choixBoisson = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Vous avez choisi un {boissons[choixBoisson - 1]}.");
+            switch (choixBoisson)
+            {
+                case 1:
+                    Console.WriteLine($"Le prix est de {prixEspresso} €.");
+                    break;
+                case 2:
+                    Console.WriteLine($"Le prix est de {prixLait} €.");
+                    break;
+                case 3:
+                    Console.WriteLine($"Le prix est de {prixCapuccino} €.");
+                    break;
+                case 4:
+                    Console.WriteLine($"Le prix est de {prixChocolatChaud} €.");
+                    break;
+                case 5:
+                    Console.WriteLine($"Le prix est de {prixCafeAuLait} €.");
+                    break;
+                case 6:
+                    Console.WriteLine($"Le prix est de {prixMokaccino} €.");
+                    break;
+                case 7:
+                    Console.WriteLine($"Le prix est de {prixThé} €.");
+                    break;
+                default:
+                    Console.WriteLine("Choix invalide.");
+                    break;
+            }
+
+            Console.WriteLine("Merci d'avoir choisi notre distributeur à café");
+            Console.ReadLine();
+        }
+    }
+
+}
